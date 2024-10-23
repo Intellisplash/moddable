629663f8eecfa60c88209beccee61436bf2d82ed
cmidgley <chris@Koose.com>
2024-10-23 11:34:20 -0400

    Remove unnecessary import

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,5 +1,4 @@
 import System_ from "embedded:io/system"
-import "embedded:provider/builtin-device"
 
 declare global {
   // NOTE: `System` is non-standard and temporary to support the IO examples. Breaking changes are possible.
